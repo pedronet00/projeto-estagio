64df536107bf177736ba6c8d8e245cd693622d6b
Pedro Neto <stabipepedro010403@gmail.com>
2024-05-14 16:42:16 -0300

    editando pagina sem conteudo

--- src/public/views/exceptions/no_content.php
+++ src/public/views/exceptions/no_content.php
@@ -1,6 +1,17 @@
-<?php 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 
-    echo "no content";
-
-?>
+<div class="card mb-3" style="width: 100%; height: 47.9vh; border: none;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="/src/public/assets/img/img-sistema/no-content.png" style="width: 400px; height: 400px;" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Ainda não há conteúdo por aqui!</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
